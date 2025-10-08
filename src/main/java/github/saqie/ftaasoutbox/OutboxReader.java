0c3d54acbd923c21e28a6115314dde026009ad5e
SaQie <Kamcio99@interia.com>
2025-10-08 18:53:43 +0200

    fix

--- src/main/java/github/saqie/ftaasoutbox/OutboxReader.java
+++ src/main/java/github/saqie/ftaasoutbox/OutboxReader.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-@Service
 @Slf4j
 @AllArgsConstructor
 class OutboxReader {
@@ -20,6 +19,7 @@ class OutboxReader {
             initialDelayString = "${outbox.initial-delay-ms:0}"
     )
     public void process() {
+        log.info("In process");
         final var unprocessedOutboxes = outboxRepository.findUnprocessedOutboxes(outboxProperties.getBatchSize());
         unprocessedOutboxes.forEach(outbox -> {
             log.info("Publishing event {}", outbox.eventId());
