2cd598f2fbe921d7a7e39feb8c9f8ddcc1fee4d3
SaQie <Kamcio99@interia.com>
2025-10-08 18:27:10 +0200

    fix

--- src/main/java/github/saqie/ftaasoutbox/OutboxAutoConfiguration.java
+++ src/main/java/github/saqie/ftaasoutbox/OutboxAutoConfiguration.java
@@ -59,11 +59,6 @@ class OutboxAutoConfiguration {
     }
 
     @Bean
-    OutboxProperties outboxProperties() {
-        return new OutboxProperties();
-    }
-
-    @Bean
     @ConditionalOnMissingBean(OutboxReader.class)
     OutboxReader outboxReader(OutboxRepository outboxRepository, OutboxProperties props, OutboxPublisher publisher) {
         return new OutboxReader(outboxRepository, props, publisher);
--- src/main/java/github/saqie/ftaasoutbox/OutboxReader.java
+++ src/main/java/github/saqie/ftaasoutbox/OutboxReader.java
@@ -1,26 +1,23 @@
 package github.saqie.ftaasoutbox;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 class OutboxReader {
 
     private final OutboxRepository outboxRepository;
     private final OutboxProperties outboxProperties;
     private final OutboxPublisher outboxPublisher;
 
-    OutboxReader(final OutboxRepository outboxRepository, final OutboxProperties outboxProperties, final OutboxPublisher outboxPublisher) {
-        this.outboxRepository = outboxRepository;
-        this.outboxProperties = outboxProperties;
-        this.outboxPublisher = outboxPublisher;
-    }
 
     @Scheduled(
-            fixedDelayString = "#{@outboxProperties.poolIntervalMs.toMillis()}",
-            initialDelayString = "#{@outboxProperties.initialDelayMs.toMillis()}"
+            fixedDelayString = "#{@outbox.poolIntervalMs.toMillis()}",
+            initialDelayString = "#{@outbox.initialDelayMs.toMillis()}"
     )
     public void process() {
         final var unprocessedOutboxes = outboxRepository.findUnprocessedOutboxes(outboxProperties.getBatchSize());
